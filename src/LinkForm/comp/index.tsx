83e5696e48750ac72238bdae165f0864ebc489d1
wangkesi <wangkesi@guorou.tv>
2021-06-04 20:20:15 +0800

    feat: link表单和toast表单

---
+++ src/LinkForm/comp/index.tsx
@@ -0,0 +1,73 @@
+import { Form, Input, Radio } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
+import React, { useEffect, useState } from 'react';
+import './index.less';
+
+interface LinkActionFormProps {
+  value: any;
+  onChange: (value: any) => void;
+}
+
+enum JumpMethod {
+  H5 = 'h5',
+  Mini = 'mini',
+}
+
+const LinkActionForm: React.FC<LinkActionFormProps> = (props) => {
+  const { value, onChange } = props;
+  const [form] = Form.useForm()
+
+
+  useEffect(() => {
+    if (value) {
+      form.setFieldsValue(JSON.parse(JSON.stringify(value)));
+    }
+  }, [value]);
+
+  const handleValues = (changedValues: any, values: any) => {
+    if (values) {
+      onChange && onChange(JSON.parse(JSON.stringify(values)))
+    }
+  };
+
+  return (
+    <div className="link-action-form">
+      <Form form={form} onValuesChange={handleValues}>
+        <Form.Item name="jumpMethod">
+          <Radio.Group>
+            {/* <Radio.Button>跳转页面</Radio.Button> */}
+            <Radio.Button value={JumpMethod.H5}>跳转H5</Radio.Button>
+            <Radio.Button value={JumpMethod.Mini}>跳转小程序</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item shouldUpdate>
+          {({ getFieldValue }) => {
+            const jumpMethod = getFieldValue(['jumpMethod']);
+            return (
+              <>
+                {jumpMethod === JumpMethod.H5 && (
+                  <Form.Item name="url">
+                    <div className="link-action-form-item">
+                      <TextArea placeholder="请输入H5路径" />
+                    </div>
+                  </Form.Item>
+                )}
+                {jumpMethod === JumpMethod.Mini && (
+                  <>
+                    <Form.Item name="id">
+                      <Input placeholder="请输入小程序id" />
+                    </Form.Item>
+                    <Form.Item name="url">
+                      <Input placeholder="请输入小程序路径" />
+                    </Form.Item>
+                  </>
+                )}
+              </>
+            );
+          }}
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+export default LinkActionForm;
---
+++ src/ToastForm/comp/index.tsx
@@ -0,0 +1,42 @@
+import { Form, Input, Radio } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
+import React, { useEffect, useState } from 'react';
+import './index.less';
+
+interface ToastActionFormProps {
+  value: any;
+  onChange: (value: any) => void;
+}
+
+enum JumpMethod {
+  H5 = 'h5',
+  Mini = 'mini',
+}
+
+const ToastActionForm: React.FC<ToastActionFormProps> = (props) => {
+  const { value, onChange } = props;
+  const [form] = Form.useForm()
+
+  useEffect(() => {
+    if (value) {
+      form.setFieldsValue(JSON.parse(JSON.stringify(value)));
+    }
+  }, [value]);
+
+  const handleValues = (changedValues: any, values: any) => {
+    if (values) {
+      onChange && onChange(JSON.parse(JSON.stringify(values)))
+    }
+  };
+
+  return (
+    <div className="link-action-form">
+      <Form form={form}  onValuesChange={handleValues}>
+        <Form.Item name="msg">
+          <Input maxLength={20}></Input>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+export default ToastActionForm;
