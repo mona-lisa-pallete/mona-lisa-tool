616b747c8e447f0fac8b2ffeabe09006262944d3
sanget <zh.sanget@gmail.com>
2021-06-17 15:32:30 +0800

    chore: 修复发布组件的脚本的问题

--- scripts/upload-widget-2-oss.sh
+++ scripts/upload-widget-2-oss.sh
@@ -1,10 +1,13 @@
 pwd=$PWD
 
-# 压缩文件
-# . ./scripts/zip-comp.sh ./src
+# 清除工作区中原有的 zip
+rm -rf $target_zip
 
 # 组件工作目录
 work_dir=$pwd/src_zip
+
+# 压缩文件
+. ./scripts/zip-comp.sh ./src
 
 element_ref=$1
 
@@ -15,7 +18,8 @@ target_zip="$work_dir/$element_ref.zip"
 
 method=POST
 
-api=http://localhost:5400/davinciapi/api/1/platform/component
+api=https://portal.guorou.net/davinciapi/api/1/platform/component
+# api=http://localhost:5400/davinciapi/api/1/platform/component
 # api=http://portalhome.uae.shensz.local/davinciapi/api/1/platform/component
 
 request_info=发布新组件
@@ -38,5 +42,7 @@ echo "API: $api"
 echo "上传的文件: $target_zip"
 
 curl --location --request $method $api \
---header 'Cookie: portal_access_token=zjy; portal_access_token.sig=wuDNxbW5HBcinqFXGxPdFSxpyyMclvIXFj1M9wAoeUU' \
+--header 'Cookie: portal_access_token=b0773763-5995-46aa-b1f6-44b3a9460b87; portal_access_token.sig=HhCwfG5jDpDUocmApxFioQLt7UzA8u6rAlUuoUwnww8' \
 --form "data=@\"$target_zip\""
+
+# --header 'Cookie: portal_access_token=zjy; portal_access_token.sig=wuDNxbW5HBcinqFXGxPdFSxpyyMclvIXFj1M9wAoeUU' \
